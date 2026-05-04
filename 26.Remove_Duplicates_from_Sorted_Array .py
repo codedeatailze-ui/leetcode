1cb8d1c5c3a0dfa68014db9f343d385fb1d61457
sajjad_br <codedeatailze@gmail.com>
2026-05-04 13:33:28 +0330

    add_input

---
+++ 26.Remove_Duplicates_from_Sorted_Array .py
@@ -0,0 +1,15 @@
+def removeDuplicates(nums: list) -> int:
+    if not nums:
+        return 0
+
+    k = 1
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
+
+
+print(removeDuplicates(list(map(int, input().split()))))
